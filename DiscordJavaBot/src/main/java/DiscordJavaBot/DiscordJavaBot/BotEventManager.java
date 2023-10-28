63e711f37466a2015cbc3e9a6a76ab02bc216166
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2023-10-28 13:02:26 +0200

    Update BotEventManager.java

--- DiscordJavaBot/src/main/java/DiscordJavaBot/DiscordJavaBot/BotEventManager.java
+++ DiscordJavaBot/src/main/java/DiscordJavaBot/DiscordJavaBot/BotEventManager.java
@@ -10,22 +10,14 @@ import net.dv8tion.jda.api.events.Event;
 
 public class BotEventManager {
 	private Set<Response>responses;
-	private String prefix;
-	
-	/*TODO: el prefix tiene que ser no alfanumerico, la id de las responses solo puede ser alfanumerico
-	*
-	*		hacer esta comprobacion desde el event listener:	validatePrefix
-	*															validateResponse
-	*
-	*/
-	
+
 	public BotEventManager(String pref) {
-		prefix=pref;
+		
 		responses=new HashSet<Response>();
 	}
 	
 	public void addResp(Response resp) {
-		//TODO: cuidado, lo que me den tiene que empezar por el prefix
+		
 		if(responses.add(resp)) {
 		System.out.println("Response to '"+resp.getId()+"' added successfully");
 		}
@@ -36,11 +28,8 @@ public class BotEventManager {
 	
 	public void removeResp(String id) {
 		Response asked=new Response(id,"This is a Mock") {
-			
 			@Override
 			public void resolve(Event event) {
-				// TODO Auto-generated method stub
-				
 			}
 		};
 		
@@ -53,42 +42,6 @@ public class BotEventManager {
 		
 		return Arrays.toString(responses.toArray());
 	}
-	
-	
-	public static void main(String[] args) {
-		BotEventManager b=new BotEventManager("/");
-		b.addResp(new Response("hello","hace algo") {
-			
-			@Override
-			public void resolve(Event event) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		b.addResp(new Response("play","hace algo") {
-			
-			@Override
-			public void resolve(Event event) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		
-		System.out.println(b.show());
-		b.removeResp("hello");
-		System.out.println(b.show());
-		
-		
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	
 }
