259a8b760c4e81da8a2e49397c7126769b1a029c
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2023-10-28 03:17:47 +0200

    Create BotEventHandler.java

---
+++ DiscordJavaBot/src/main/java/DiscordJavaBot/DiscordJavaBot/BotEventHandler.java
@@ -0,0 +1,22 @@
+package DiscordJavaBot.DiscordJavaBot;
+
+import java.util.ArrayList;
+
+public class BotEventHandler {
+	private ArrayList<Response>responses;
+	private String prefix;
+	
+	public BotEventHandler(String pref) {
+		
+	}
+	
+	public void addResp() {}
+	
+	
+	public void removeResp() {}
+	
+	public String show() {
+		
+		return null;
+	}
+}
